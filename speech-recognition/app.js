fd58c903ada6c03782f278ed0776fbe84236274d
lzx <2422005947@qq.com>
2024-10-27 09:19:10 +0800

    v3基本框架

--- speech-recognition/app.js
+++ speech-recognition/app.js
@@ -9,10 +9,10 @@ var spawn = require('child_process').spawn;
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // 上传文件的存储路径
+        cb(null, 'uploads/'); 
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname)); // 使用时间戳作为文件名
+        cb(null, Date.now() + path.extname(file.originalname)); 
     }
 });
 const upload = multer({ storage: storage });
@@ -20,43 +20,37 @@ const upload = multer({ storage: storage });
 app.get('/', (req, res) => {
     res.render("page")
 });
-app.post('/python1', upload.single('audio'), (req, res) => {//调用本地.py，直接获得返回值，.py文件不用访问server
-        console.log(req.file)
-    const py = spawn('D:\\PY\\python.exe', ['D:\\pycharm\\py_projects\\pythonProject6\\test.py',  'C: \Users\24220\source\repos\speech - recognition\speech - recognition/uploads/' +req.file.filename]);
+app.post('/python', upload.single('audio'), (req, res) => {
+    console.log(req.file.filename)
+    const py = spawn('D:\\PY\\python.exe', ['D:\\pycharm\\py_projects\\pythonProject6\\test.py', 'C:\\Users\\24220\\source\\repos\\speech-recognition\\speech-recognition\\uploads']);
 
-
-    fs.unlink(path.resolve('uploads/' + req.file.filename), (err) => {
+    py.stdout.on('data', function (res) {//等python返回再删除，防止python还未完成就删了
+        let data = res.toString();
+        console.log('stdout: ', data)
+        })
+     /*fs.unlink(path.resolve('uploads/' + req.file.filename), (err) => {//删除语音文件wav
         if (err) {
             console.error(err);
             return res.status(500).send('Error deleting the file');
         }
-    });
+     });*/
 
 
-    py.stdout.on('data', function (res) {
-        let data = res.toString();
-        console.log('stdout: ', data)
-    })
-    var row =0
-        //result.slice(0, 1)
-    var col =1
-        //result.slice(1)
-    res.json({row:row,col:col});
-});
-app.post('/python2', upload.single('audio'),(req, res) => {
-    const py = spawn('D:\\PY\\python.exe', ['D:\\pycharm\\py_projects\\pythonProject6\\test.py','C: \Users\24220\source\repos\speech - recognition\speech - recognition/uploads/'+ req.file.filename]);
-    fs.unlink(path.resolve('uploads/' + req.file.filename), (err) => {
+    /*fs.readFile('C:\\Users\\24220\\Documents\\WeChat Files\\wxid_fkvcjxsw3ncl22\\FileStorage\\File\\2024-10\\uploads\\' + req.file.filename.slice(0, -3) + 'txt', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
-            return res.status(500).send('Error deleting the file');
+            return res.status(500).send('Error reading file');
         }
+        res.json({ content: data }); 
     });
-    py.stdout.on('data', function (res) {
-        let data = res.toString();
-        console.log('stdout: ', data)
-        //res.end(data);
-    })
     
+    fs.unlink(path.resolve('uploads/' + req.file.filename.slice(0, -3) + 'txt'), (err) => {//删除结果文件txt
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error deleting the file');
+        }
+    });*/
+
 });
 
 
